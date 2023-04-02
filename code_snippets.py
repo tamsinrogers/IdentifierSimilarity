de4b0a768e14f54deedc3404a5dcc671fda9ffc0
Milo Lani-Caputo <mlanicaputo@gmail.com>
2023-04-02 18:56:18 -0400

    Create code snippets file

---
+++ code_snippets.py
@@ -0,0 +1,3 @@
+# Code snippets to be used in the survey. Snippets should include either
+# similar or different identifiers, validated using objective criteria
+# from an IS paper. The relevant lines should be flagged with a comment
