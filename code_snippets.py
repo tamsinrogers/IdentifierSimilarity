50c6b52adbb674571fb3f05fc64bfb5ae21008d2
Milo Lani-Caputo <mlanicaputo@gmail.com>
2023-04-05 22:50:21 -0400

    Add good identifier version of func6

--- code_snippets.py
+++ code_snippets.py
@@ -263,6 +263,24 @@ print("\n\n")
 # SNIPPET 6: SMALL STRING, SHORT STRING
 # SEMANTIC
 def func6_b(str_list):
+    five_str = False
+    ten_str = False
+    for little_string in str_list:
+        if len(little_string) < 5:
+            five_str = True
+        else:
+            if len(little_string) < 10:
+                ten_str = True
+        if five_str:
+            print(little_string[0])
+        elif ten_str:
+            print(five_str)
+        else:
+            print(little_string[1])
+        five_str = False
+        ten_str = False
+
+def func6_b(str_list):
     short_string = False
     small_string = False
     for little_string in str_list:
