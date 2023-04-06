ed3fde8a41e6b4c0217b1eb79a6e289bc5dd4b00
Milo Lani-Caputo <mlanicaputo@gmail.com>
2023-04-05 21:54:49 -0400

    Add semantic snippet 5

--- code_snippets.py
+++ code_snippets.py
@@ -159,3 +159,31 @@ print(
         ["zero", 1, "two", 4.5, 8, 16, "thirty-two", 64.0]
     )
 )
+
+
+
+
+# -------------------------------------------------------------------------- #
+# SNIPPET 5: SMALL STRING, SHORT STRING
+# SEMANTIC
+def func6(str_list):
+    short_string = False
+    small_string = False
+    for little_string in str_list:
+        if len(little_string) < 5:
+            short_string = True
+        else:
+            if len(little_string) < 10:
+                small_string = True
+        if short_string:
+            print(little_string[0])
+        elif small_string:
+            print(short_string)
+        else:
+            print(little_string[1])
+        short_string = False
+        small_string = False
+
+func6(
+    ["treble", "bass", "microphone", "amplifier", "guitar", "glasses", "anyone"]
+)
