14a3028e28275b9a5847eb177d139d6f96866419
Milo Lani-Caputo <mlanicaputo@gmail.com>
2023-04-05 21:01:34 -0400

    Add function semantic

--- code_snippets.py
+++ code_snippets.py
@@ -123,5 +123,39 @@ print(
 
 
 # -------------------------------------------------------------------------- #
-# SNIPPET 5: 
+# SNIPPET 5: LIST VS ARRAY
 # SEMANTIC
+# list gets only int
+# array gets only strs
+# if it's a float, add zero to other list
+def func5(input_list, input_array):
+    i = 0
+    while i < len(input_list):
+        print(i)
+        if type(input_list[i]) == str:
+            input_array.append(input_list[i])
+            input_list.remove(input_list[i])
+        elif type(input_list[i]) == float:
+            input_array.append("0")
+            input_list.remove(input_list[i])
+        else:
+            i += 1
+    j = 0
+    while j < len(input_array):
+        if type(input_array[j]) == int:
+            input_list.append(input_array[j])
+            input_array.remove(input_array[j])
+        elif type(input_array[j]) == float:
+            input_list.append(0)
+            input_array.remove(input_array[j])
+        else:
+            j += 1
+    
+    return input_list, input_array
+
+print(
+    func5(
+        [2, "three", "four", 5, 6.7, "seven", "eight", 9, "ten"],
+        ["zero", 1, "two", 4.5, 8, 16, "thirty-two", 64.0]
+    )
+)
